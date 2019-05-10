6c178be61633aa5e3d10a9f7f449345dd6e6b6b6
Philip Johnson <phil7j@gmail.com>
2019-05-10 09:15:14 -0700

    MVP complete

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,36 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
+// let CuboidMaker = function(props){
+//     this.length = props.length;
+//     this.width = props.width;
+//     this.height = props.height;
+//   }
+//   CuboidMaker.prototype.volume = function(){
+//     return this.length * this.width * this.height;
+//   }
+//   CuboidMaker.prototype.surfaceArea = function(){
+//     return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+//   }
 
+class CuboidMaker {
+    constructor(props){
+        this.length = props.length;
+        this.width = props.width;
+        this.height = props.height;
+    }
+    volume(){
+        return this.length * this.width * this.height;
+    }
+    surfaceArea(){
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+    }
+}
+
+
+  const cuboid = new CuboidMaker ({
+    length: 4,
+    width: 5,
+    height: 5
+  });
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
